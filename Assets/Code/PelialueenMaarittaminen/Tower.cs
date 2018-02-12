45cb705ec4b9eb5cd91fb652cdd7b763d1314599
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-02-12 11:55:45 +0200

    Removed unused scenes and scripts. Tower now jumps with player.

--- Assets/Code/PelialueenMaarittaminen/Tower.cs
+++ Assets/Code/PelialueenMaarittaminen/Tower.cs
@@ -18,9 +18,13 @@ public class Tower : MonoBehaviour {
     
     // Adds a chicken to the tower.
     public void AddChicken() {
+        //Calculate the new chicken's position by the player's pos and the amount of chicken in the tower.
         Vector3 pos = transform.position + ((transform.up * chickenCount) + new Vector3(0, 1.5f, 0));
 
+        //Instantiate a clone of the chicken prefab, so we can Destroy() it later.
         GameObject cloneChicken = Instantiate(chicken, pos, Quaternion.identity) as GameObject;
+        //Parent it to the player so it moves with the player.
+        cloneChicken.transform.parent = transform;
 
         tower.Add(cloneChicken);
         chickenCount = tower.Count;
@@ -29,6 +33,9 @@ public class Tower : MonoBehaviour {
     // Removes a chicken from the tower, returns the removed chicken object.
     public GameObject RemoveChicken() {
         GameObject removedChicken = tower[chickenCount - 1].gameObject;
+
+        //Remove parent link before Removing.
+        removedChicken.transform.parent = null;
 
         tower.Remove(removedChicken);
         chickenCount--;
