068aad7e76a71fe006f11316195a9b6b931d8ae8
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-02-26 18:13:45 +0200

    Little reorganising in unity. Added v1 chicken spawner, added v1 haystack and destruction.

--- Assets/Code/ChickenSpawner.cs
+++ Assets/Code/ChickenSpawner.cs
@@ -4,17 +4,50 @@ using UnityEngine;
 
 public class ChickenSpawner : MonoBehaviour {
 
+    public GameObject[] spawnerRoute = new GameObject[4];
     public GameObject prefabToSpawn;
     public float spawnTimer;
+    private float timer;
+    public float speed;
+    public int i = 0;
+    
+    Vector3 newDestination;
 
-
-	// Use this for initialization
-	void Start () {
-	    	
-	}
+    // Use this for initialization
+    void Start () {
+        newDestination = spawnerRoute[0].transform.position;
+        timer = spawnTimer;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        
+        if (Vector3.Distance(transform.position, newDestination) < 1f)
+        {
+            if (i == spawnerRoute.Length - 1)
+            {
+                i = 0;
+            }
+            else
+            {
+                i++;
+            }
+
+            newDestination = spawnerRoute[i].transform.position;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, newDestination, speed * Time.deltaTime);
+        timer -= Time.deltaTime;
+
+        if(timer < 0)
+        {
+            SpawnChicken();
+            timer = spawnTimer;
+        }
 	}
+
+    void SpawnChicken()
+    {
+        GameObject cloneChicken = Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
+    }
 }
