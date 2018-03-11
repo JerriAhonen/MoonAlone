34d928160d1fd769e6cdd3724df593b660cdf249
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-03-11 10:30:52 +0200

    Cleaned un-needed scenes. Menu Navigation prototyping.

--- Assets/Code/MenuNavigation.cs
+++ Assets/Code/MenuNavigation.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class MenuNavigation : MonoBehaviour {
 
+    public Camera camera;
+
     public GameObject[] buttons;
     public int index;
 
-    public GameObject selectorCubes;
-    public GameObject[] buttonPositions = new GameObject[4];
+    //public GameObject selectorCubes;
+    //public GameObject[] buttonPositions = new GameObject[4];
 
     public string toggleAxis;
     public float selectionCooldown;
@@ -16,12 +18,20 @@ public class MenuNavigation : MonoBehaviour {
     private bool axisInUse = false;
     public string confirmButton;
     private bool optionConfirmed = false;
+
+    public Transform cameraPosMenu;
+    public Transform cameraPosCharSel;
+
     // Use this for initialization
     void Start () {
         buttons = new GameObject[transform.childCount];
 
         for (int i = 0; i < transform.childCount; i++)
             buttons[i] = transform.GetChild(i).gameObject;
+
+        buttons[index].transform.position = buttons[index].transform.position - Vector3.forward / 2;
+
+        camera.GetComponent<CameraController>().MoveCamera(cameraPosMenu);      // Move camera to Menu view
     }
 	
 	// Update is called once per frame
@@ -66,28 +76,31 @@ public class MenuNavigation : MonoBehaviour {
 
     public void ToggleUp()
     {
-        
+        buttons[index].transform.position = buttons[index].transform.position + Vector3.forward / 2;
+
         index--;
         if (index < 0)
             index = buttons.Length - 1;
 
-        selectorCubes.transform.position = buttonPositions[index].transform.position;
+        buttons[index].transform.position = buttons[index].transform.position - Vector3.forward / 2;
 
     }
 
     public void ToggleDown()
     {
-        
+        buttons[index].transform.position = buttons[index].transform.position + Vector3.forward / 2;
+
         index++;
         if (index == buttons.Length)
             index = 0;
 
-        selectorCubes.transform.position = buttonPositions[index].transform.position;
+        buttons[index].transform.position = buttons[index].transform.position - Vector3.forward / 2;
     }
 
     public void Confirm()
     {
         optionConfirmed = true;
+        camera.GetComponent<CameraController>().MoveCamera(cameraPosCharSel);      // Move camera to Char Sel view
     }
 
     public void EnterMainMenu()
