22970d379cd40e23c387c718410ce7bcf973b2d4
Suvi <suvi.kemppainen@cs.tamk.fi>
2018-03-20 15:12:12 +0200

    Fuckedup flying.

--- Assets/Code/Chicken.cs
+++ Assets/Code/Chicken.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Chicken : MonoBehaviour
 {
-
     public float numberInTower;
     public float yPos;
     public float wanderDistance;
@@ -28,10 +27,9 @@ public class Chicken : MonoBehaviour
 
     public float gravity = 16.0f;
     public float verticalVelocity;
-    public float downwardsFallMultiplier;
+    public float upwardsMultiplier;
     public float throwForce = 8f;
-    public float throwHeight = 5f;
-
+    private float _flyTime;
     public bool isThrown = false;
     public bool isFalling = false;
     public string _pickUpLayer = "PickUp";
@@ -112,44 +110,48 @@ public class Chicken : MonoBehaviour
 
     // The chicken flies through the air.
     void Fly() {
-        verticalVelocity = throwForce;
+        _flyTime += Time.deltaTime;
         
         if (verticalVelocity > -50) {                                           // No need to go smaller than this
             verticalVelocity -= gravity * Time.deltaTime;                       // Always decrease verticalVelocity
 
-            if (verticalVelocity < 0) {                                         // Fall faster than go up
-                verticalVelocity -= gravity * downwardsFallMultiplier * Time.deltaTime;
+            if (verticalVelocity > 0) {                                         // Fall faster than go up
+                verticalVelocity += gravity * upwardsMultiplier * Time.deltaTime;
             }
         }
 
         Vector3 verticalMovement = new Vector3(0, verticalVelocity, 0);         // Get vertical movement in Vector3 form
 
-        transform.position += (transform.up * throwHeight * Time.deltaTime);
-        transform.position -= (transform.up * gravity * Time.deltaTime);
-
         transform.position += (transform.forward * throwForce * Time.deltaTime);
-        transform.position += verticalMovement * Time.deltaTime;
+
+        if (_flyTime < 0.1f) {
+            transform.position += (verticalMovement * Time.deltaTime);
+        } else {
+            transform.position -= (verticalMovement * Time.deltaTime);
+        }
         
         if (transform.position.y < 0.1f) {
             isThrown = false;
             isFalling = false;
+            _flyTime = 0;
         }
     }
 
     // Set the parameters for flight (throw / fall).
     public void SetFlight(bool toBeThrown, bool flyFar) {
+        verticalVelocity = throwForce;
+        _flyTime = 0;
+        
         if (toBeThrown) {
             mainCamera = GameObject.Find("Main Camera");
             // TODO: Make throw faster
             isThrown = true;
             
             if (flyFar) {
-                throwForce = 8f;
-                throwHeight = 5f;
+                throwForce = 20f;
                 gravity = 16f;
             } else {
-                throwForce = 5f;
-                throwHeight = 3f;
+                throwForce = 15f;
                 gravity = 16f;
             }
             
@@ -157,7 +159,6 @@ public class Chicken : MonoBehaviour
             // TODO: Make falling look like falling instead of an explosion, doesn't really use Fly() as much as just Rigidbodies being Rigidbodies at the moment.
             isFalling = true;
             throwForce = 0f;
-            throwHeight = 0f;
         }
     }
 
