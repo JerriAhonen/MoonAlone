e2267f8d5f92e339c8583e66f7b139ce07b02c47
WPK\c5jeraho <jerri.ahonen@gmail.com>
2018-04-05 13:58:59 +0300

    imported Kake's particle package. Added FMODStudioCache. Again.

--- Assets/Scenes/Debug/Code/DebugGameManager.cs
+++ Assets/Scenes/Debug/Code/DebugGameManager.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DebugGameManager : MonoBehaviour {
 
+
+	public bool[] scenes = new bool[5];
+	public int currentScene = 0;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +14,32 @@ public class DebugGameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown(KeyCode.Space)) {
+                Debug.Log("Switch scene");
+				currentScene++;
+				if (currentScene == scenes.Length) {
+					currentScene = 0;
+				}
+				
+                ChangeScene(currentScene);
+            }
 	}
+
+	public void ChangeScene(int newScene)                                       // Changes the scene boolean
+    {
+        // bool mainMenu;          // 0
+        // bool characterSelect;   // 1
+        // bool round;             // 2
+        // bool roundScore;        // 3
+        // bool gameOver;          // 4
+
+        for (int i = 0; i < scenes.Length; i++)
+        {
+            if (scenes[i] == true)
+                scenes[i] = false;
+        }
+
+        scenes[newScene] = true;
+		Debug.Log("Current scene = " + newScene);
+    }
 }
