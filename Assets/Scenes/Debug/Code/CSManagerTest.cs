544f2a2d11c5e30f050aa800646742f422c5dec3
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-04-07 02:17:28 +0300

    Bug fixed the debug scripts.

---
+++ Assets/Scenes/Debug/Code/CSManagerTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceHenHassle
+{
+    public class CSManagerTest : MonoBehaviour
+    {
+
+        private int numOfPlayers;
+        private int numOfReady;
+
+        public CSTest cs1;
+        public CSTest cs2;
+        public CSTest cs3;
+        public CSTest cs4;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
+
+
+
---
+++ Assets/Scenes/Debug/Code/CameraControllerTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SpaceHenHassle
+{
+    public class CameraControllerTest : MonoBehaviour
+    {
+
+        private Transform targetTransform;
+        public bool cameraMoved = false;
+        public Transform MMcamPos;
+        public Transform CScamPos;
+        public Transform RcamPos;
+        public Transform RScamPos;
+        public Transform GOcamPos;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (cameraMoved)
+            {
+                transform.position = Vector3.Slerp(transform.position,
+                    targetTransform.position, Time.deltaTime);
+                transform.rotation = Quaternion.Slerp(transform.rotation,
+                    targetTransform.rotation, Time.deltaTime);
+
+            }
+        }
+
+        public void MoveCamera(int i)
+        {
+            cameraMoved = true;
+
+            //Update the camera transform location when changing scenes
+            switch (i)
+            {
+                case 0:
+                    targetTransform = MMcamPos;
+                    break;
+                case 1:
+                    targetTransform = CScamPos;
+                    break;
+                case 2:
+                    targetTransform = RcamPos;
+                    break;
+                case 3:
+                    targetTransform = RScamPos;
+                    break;
+                case 4:
+                    targetTransform = GOcamPos;
+                    break;
+            }
+        }
+    }
+}
+
