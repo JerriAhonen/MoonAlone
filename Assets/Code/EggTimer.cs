aca17cd484eb6c4a3936079ef625825516f7660a
Cagez <kallevikstrom@hotmail.com>
2018-04-24 16:34:53 +0300

    Kalle teki kaikenlaista valoilla, ja sitten koodattiin yhdessä vähä eggtimeria. Kalle pyys puskeen nää, kun se lähti jos poies.

--- Assets/Code/EggTimer.cs
+++ Assets/Code/EggTimer.cs
@@ -3,28 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EggTimer : MonoBehaviour {
-
     public float time;
     public float speed;
 
+    public float shakeSpeed;
+    private float shakeTime;
+
+    public bool shakeRight = true;
+    public bool shakeLeft = false;
+
 	// Use this for initialization
 	void Start () {
-        time = 60f;
-        speed = 10f;
+        shakeTime = 0.01f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         time -= Time.deltaTime;
-
-        Rotate();
+        if (time >= 0)
+        {
+            Rotate();
+        }
+        else {
+            Shake();
+        }
+        
 
         //Debug.Log(time);
 	}
 
     void Rotate()
     {
-        //transform.rotation = Vector3.up * time * 60;
+       
         transform.Rotate(Vector3.forward, speed * Time.deltaTime);
+    }
+
+    void Shake() {
+
+        if (shakeRight)
+        {
+            transform.Rotate(Vector3.forward, -shakeSpeed * Time.deltaTime);
+            shakeTime -= Time.deltaTime;
+            if (shakeTime < 0)
+            {
+                shakeTime = 0.01f;
+                shakeLeft = true;
+                shakeRight = false;
+            }
+        }
+        else if (shakeLeft)
+        {
+            transform.Rotate(Vector3.forward, shakeSpeed * Time.deltaTime);
+            shakeTime -= Time.deltaTime;
+            if (shakeTime < 0)
+            {
+                shakeTime = 0.01f;
+                shakeLeft = false;
+                shakeRight = true;
+            }
+        }
     }
 }
