984a9beb849e93624a3e9b226b1d72e6d0b751ea
JerriAhonen <jerri.ahonen@cs.tamk.fi>
2018-04-28 18:29:16 +0300

    Adjusted Player speed. Redid GetRoundScore(); need testing tho

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -16,7 +16,6 @@ public class GameManager : MonoBehaviour {
     public int noPlayerCount = 4;                                               // Num of players not playing
 
     public TextMeshPro timerText;                                               // Timer displayer during round
-    //public TextMeshPro winnerText;                                              // Text displayed after round
     public TextMeshPro nextRound;                                               // Text saying what happens next after round finished
 
     public float setTimer = 60.0f;                                              // Round lenght in seconds
@@ -43,13 +42,10 @@ public class GameManager : MonoBehaviour {
 
     private void Start()
     {
-        //camera.GetComponent<CameraController>().MoveCamera(cameraPosCharacterSelection);
-
         curRoundNum = PlayerPrefs.GetInt("CurrentRoundNumber");
         numberOfPlayers = PlayerPrefs.GetInt("NumberOfPlayers");
         timer = setTimer;
-
-        //EnablePlayerGOs(numberOfPlayers);
+        
         displayTransitionText = false;
 
         levelMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Music/level");
@@ -108,7 +104,6 @@ public class GameManager : MonoBehaviour {
             timeLeft = System.Convert.ToInt32(timer % 60);                  // Reset TimeLeft
 
             timerText.text = "Time's up!";
-            //winnerText.text = "Player " + playerNum + " wins with " + winningScore + " chickens!";
 
             Debug.Log("Started CooldownTimer Coroutine");
             StartCoroutine(StartCooldownTimer(5));
@@ -132,21 +127,6 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-    // Adds an Animator, AnimationController and an Avatar to the player
-    //public void addAnimators(int index)
-    //{
-
-    //    for (int i = 0; i < numberOfPlayers; i++)
-    //    {
-    //        Animator animator = cs[index].transform.getchild(cs[index].selectedcharacter).gameobject.addcomponent<Animator>() as Animator;
-    //    }
-
-    //    // finds the selected model through cs (characterselector) and add's the animator component to it.
-        
-    //    animator.runtimeanimatorcontroller = runtimeanimatorcontroller;
-    //    animator.avatar = avatar;
-    //}
-
     public void AddAnimators(int num)
     {
         for (int i = 0; i < num; i++)
@@ -157,45 +137,92 @@ public class GameManager : MonoBehaviour {
         }
     }
 
-        //TODO: MAKE THIS WORK
-    public void GetRoundScore(){
+    public void GetRoundScore()
+    {
+        int[] scores = new int[numberOfPlayers];
 
-        int[] scores = new int[4];
-
-        // Fill scores[] with players' chicken counts
-        for (int i = 0; i < players.Length; i++)
+        for (int i = 0; i < scores.Length; i++)
         {
             if (players[i].activeSelf)
                 scores[i] = players[i].GetComponent<Tower>().chickenCount;
+            else
+                Debug.Log("GetRoundScore(); Player" + i + " isn't active");
         }
 
         System.Array.Sort(scores);
 
-        for (int i = 0; i < players.Length; i++)
+        for (int i = 0; i < scores.Length; i++)
         {
-            if (players[i].activeSelf) {
-                if ( players[i].GetComponent<Tower>().chickenCount == 0) {
-                    players[i].GetComponent<Player>().score = 0;
-                } else if (players[i].GetComponent<Tower>().chickenCount == scores[3]) {
-                    players[i].GetComponent<Player>().score = readyCount - 1;
-                } else if (players[i].GetComponent<Tower>().chickenCount == scores[2]) {
-                    players[i].GetComponent<Player>().score = readyCount - 2;
-                } else if (players[i].GetComponent<Tower>().chickenCount == scores[1]) {
-                    players[i].GetComponent<Player>().score = readyCount - 3;
-                } else if (players[i].GetComponent<Tower>().chickenCount == scores[0]) {
-                    players[i].GetComponent<Player>().score = readyCount - 4;
-                }
+            string playerScore = "Player" + i + "Score";
+
+            if (players[i].GetComponent<Tower>().chickenCount == 0)
+            {
+                PlayerPrefs.SetInt(playerScore, PlayerPrefs.GetInt(playerScore) + 0);
+            }
+            else if (players[i].GetComponent<Tower>().chickenCount == scores[scores.Length - 1])
+            {
+                PlayerPrefs.SetInt(playerScore, PlayerPrefs.GetInt(playerScore) + 3);
+            }
+            else if (players[i].GetComponent<Tower>().chickenCount == scores[scores.Length - 2])
+            {
+                PlayerPrefs.SetInt(playerScore, PlayerPrefs.GetInt(playerScore) + 2);
+            }
+            else if (players[i].GetComponent<Tower>().chickenCount == scores[scores.Length - 3])
+            {
+                PlayerPrefs.SetInt(playerScore, PlayerPrefs.GetInt(playerScore) + 1);
+            }
+            else if (players[i].GetComponent<Tower>().chickenCount == scores[scores.Length - 4])
+            {
+                PlayerPrefs.SetInt(playerScore, PlayerPrefs.GetInt(playerScore) + 0);
             }
         }
-        
+
         for (int i = 0; i < playerScores.Length; i++)
         {
-            playerScores[i] += players[i].GetComponent<Player>().score;
+            playerScores[i] = PlayerPrefs.GetInt("Player" + i + "Score");
         }
-
     }
 
-    private void EndRound()
+
+        //TODO: MAKE THIS WORK
+        //public void GetRoundScore(){
+
+        //    int[] scores = new int[4];
+
+        //    // Fill scores[] with players' chicken counts
+        //    for (int i = 0; i < players.Length; i++)
+        //    {
+        //        if (players[i].activeSelf)
+        //            scores[i] = players[i].GetComponent<Tower>().chickenCount;
+        //    }
+
+        //    System.Array.Sort(scores);
+
+        //    for (int i = 0; i < players.Length; i++)
+        //    {
+        //        if (players[i].activeSelf) {
+        //            if ( players[i].GetComponent<Tower>().chickenCount == 0) {
+        //                players[i].GetComponent<Player>().score = 0;
+        //            } else if (players[i].GetComponent<Tower>().chickenCount == scores[3]) {
+        //                players[i].GetComponent<Player>().score = readyCount - 1;
+        //            } else if (players[i].GetComponent<Tower>().chickenCount == scores[2]) {
+        //                players[i].GetComponent<Player>().score = readyCount - 2;
+        //            } else if (players[i].GetComponent<Tower>().chickenCount == scores[1]) {
+        //                players[i].GetComponent<Player>().score = readyCount - 3;
+        //            } else if (players[i].GetComponent<Tower>().chickenCount == scores[0]) {
+        //                players[i].GetComponent<Player>().score = readyCount - 4;
+        //            }
+        //        }
+        //    }
+
+        //    for (int i = 0; i < playerScores.Length; i++)
+        //    {
+        //        playerScores[i] += players[i].GetComponent<Player>().score;
+        //    }
+
+        //}
+
+        private void EndRound()
     {
         if(PlayerPrefs.GetInt("CurrentRoundNumber") < NumOfRounds)
         {
@@ -238,132 +265,5 @@ public class GameManager : MonoBehaviour {
             Debug.Log("CooldownTimer Finished!");
             cooldownTimerFinished = true;
         }
-
     }
-
-    //void StartGame(int playerCount)                                                 // Player position and scripts and camera
-    //{
-    //    for (int i = 0; i < playerCount; i++)
-    //    {
-    //        players[i].transform.position = spawnPoints[i].transform.position;      // Teleport to starting position 
-    //        players[i].GetComponent<Player>().enabled = true;                       // Enable Player script
-    //    }
-
-    //    //camera.GetComponent<CameraController>().MoveCamera(cameraPosGameplay);      // Move camera to Game view
-    //}
-
-    //-------OLD CODE-------\\
-
-    //TODO: Make this work
-    //public bool[] chosenCharacters = new bool[6];                             // List of taken players during char selection
-
-    //public CharacterSelection[] cs = new CharacterSelection[4];               // List of all character selectors
-    //public GameObject[] spawnPoints = new GameObject[4];                        // List of player spawn points
-    //public bool[] scenes = new bool[5];                                         // <- Not yet in use
-
-    //public Camera camera;                                                       // The Main Camera
-    //public Transform cameraPosMainMenu;                                         // Pos of camera during menu
-    //public Transform cameraPosCharacterSelection;                               // Pos of camera during char selection
-    //public Transform cameraPosGameplay;                                         // Pos of camera during round
-
-    // Disable all player controls during character selection.
-    //DisablePlayerControls();
-
-    //foreach (var cs in cs)
-    //{
-    //    cs.UseTestGameManager = false;
-    //}
-
-    //scenes[0] = true;                                                       // Set the scene to Main Menu <- Not yet in use
-
-
-    // int playerNum = 0;
-    // int playerScore = 0;
-
-    // int i = 0;
-    // foreach (var player in players)                                     // Go through all players
-    // {
-    //     if (player.activeSelf == true)                                   // Check if player active
-    //     {
-    //         i++;                                                        // Start counting from P1
-    //         playerScore = player.GetComponentInChildren<Tower>().chickenCount;
-    //         if (playerScore > winningScore)                             // Find highest chicken count
-    //         {
-    //             winningScore = playerScore;
-    //             playerNum = i;
-    //         }
-    //     }
-    // }
-
-
-    // Logic for when CHARACTER SELECTION RUNNING
-    //else
-    //{
-    //    if (readyCount >= NumOfPlayersNeededToStartGame_DEBUG && noPlayerCount == (4 - readyCount))
-    //    {
-    //        StartGame(readyCount);
-    //        roundStarted = true;
-    //        roundFinished = false;
-
-    //        timerText.text = "";
-    //        winnerText.text = "";
-
-    //        // Deactivate all players that are not needed
-    //        // TODO: Clean this up.
-
-    //        switch (noPlayerCount)
-    //        {
-    //            case 0:
-    //                AddAnimator(3);
-    //                AddAnimator(2);
-    //                AddAnimator(1);
-    //                AddAnimator(0);
-    //                break;
-    //            case 1:
-    //                players[3].SetActive(false);
-    //                AddAnimator(2);
-    //                AddAnimator(1);
-    //                AddAnimator(0);
-    //                break;
-    //            case 2:
-    //                players[3].SetActive(false);
-    //                players[2].SetActive(false);
-    //                AddAnimator(1);
-    //                AddAnimator(0);
-    //                break;
-    //            case 3:
-    //                players[3].SetActive(false);
-    //                players[2].SetActive(false);
-    //                players[1].SetActive(false);
-    //                AddAnimator(0);
-    //                break;
-    //        }
-    //    }
-    //}
-
-
-
-
-
-    //private void DisablePlayerControls()                                            // Disable player GameObject
-    //{
-    //    foreach (var player in players)
-    //    {
-    //        player.GetComponent<Player>().enabled = false;                          
-    //    }
-    //}
-
-    
-
-    //public void ChangeScene(int from, int to)                                       // Changes the scene boolean <- Not yet in use
-    //{
-    //    // bool mainMenu;          // 0
-    //    // bool characterSelect;   // 1
-    //    // bool round;             // 2
-    //    // bool roundScore;        // 3
-    //    // bool gameOver;          // 4
-
-    //    scenes[from] = false;
-    //    scenes[to] = true;
-    //}
 }
