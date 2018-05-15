cd58e4199c6e8f4b4cc8cde9fbeb252e20fcedc3
WPK\c5jeraho <jerri.ahonen@gmail.com>
2018-05-15 10:59:05 +0300

    next to nothing.

--- Assets/Code/RoundScore.cs
+++ Assets/Code/RoundScore.cs
@@ -99,4 +99,10 @@ public class RoundScore : MonoBehaviour {
                 break;
         }
     }
+
+    private void OnApplicationQuit()
+    {
+        
+        Debug.Log("Reset PlayerPrefs LastRoundScores");
+    }
 }
